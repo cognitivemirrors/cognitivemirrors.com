d69a14ceedeaa9579fd27d3db84fdb320a10e034
Kevin Chan <kevinmchan@live.com>
2025-07-12 17:45:24 -0400

    Fix canvas scaling and draw a circle

--- www/dist/main.js
+++ www/dist/main.js
@@ -1,4 +1,11 @@
 // www/ts/main.ts
+function drawCircle(x, y, radius, ctx) {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, 2 * Math.PI);
+  ctx.fillStyle = "#222";
+  ctx.fill();
+  ctx.stroke();
+}
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.querySelector(
     "#exhibits-container"
@@ -7,13 +14,18 @@ document.addEventListener("DOMContentLoaded", () => {
   canvas.id = "exhibit-canvas";
   container.appendChild(canvas);
   const ctx = canvas.getContext("2d");
+  const computedStyles = globalThis.getComputedStyle(canvas);
+  const canvas_style_width = computedStyles.getPropertyValue("width");
+  const canvas_style_height = computedStyles.getPropertyValue("height");
+  const scale = globalThis.devicePixelRatio;
+  canvas.width = scale * parseInt(canvas_style_width);
+  canvas.height = scale * parseInt(canvas_style_height);
   ctx.fillStyle = "grey";
-  const canvas_width = canvas.width;
-  const canvas_height = canvas.height;
   ctx.fillRect(
-    canvas_width * 0.02,
-    canvas_height * 0.02,
-    canvas_width * 0.96,
-    canvas_height * 0.96
+    canvas.width * 0.02,
+    canvas.height * 0.02,
+    canvas.width * 0.96,
+    canvas.height * 0.96
   );
+  drawCircle(canvas.width * 0.5, canvas.width * 0.5, canvas.width * 0.01, ctx);
 });
